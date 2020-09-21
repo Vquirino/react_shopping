4751322bd28358bf4abb4065e53bd7cfce739a4f
Vquirino <v.quirino80@gmail.com>
2020-09-20 21:48:07 -0300

    projeto Shopping com Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './index.css';
 import HomeView from './views/Home.view';
-import store from './store/store'
-import { Provider } from 'react-redux'
+
+import { store, persistedStore } from './store/store'
+import { PersistGate } from 'redux-persist/es/integration/react'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HomeView />
+      <PersistGate persistor={persistedStore}>
+        <HomeView />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
